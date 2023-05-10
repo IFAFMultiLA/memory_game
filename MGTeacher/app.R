765770acbdb5ef64886b058bc2a2886b05d6fae0
Markus Konrad <post@mkonrad.net>
2023-05-10 11:35:53 +0200

    deleting sessions

--- MGTeacher/app.R
+++ MGTeacher/app.R
@@ -4,6 +4,7 @@ library(yaml)
 library(stringi)
 
 
+SESS_ID_CODE_LENGTH <- 8
 SESS_DIR <- fs::path_abs(here("..", "sessions"))
 stopifnot("the sessions directory must exist" = fs::is_dir(SESS_DIR))
 
@@ -23,21 +24,56 @@ ui <- fluidPage(
     sidebarLayout(
         sidebarPanel(
             uiOutput("sessionsList"),
-            actionButton("createSession", "Create a new session")
+            actionButton("createSession", "Create a new session", class = "btn-success")
         ),
         mainPanel(
-           uiOutput("activeSession")
+            conditionalPanel("input.sessionsSelect",
+                actionButton("deleteSession", "Delete this session", class = "btn-danger", style = "float: right"),
+                h1(textOutput("activeSessionTitle")),
+                uiOutput("activeSessionContent")
+            )
         )
     )
 )
 
 server <- function(input, output) {
-    state <- reactiveValues(sess = NULL)
+    state <- reactiveValues(
+        sess = NULL,
+        available_sessions = character()
+    )
+
+    updateAvailSessions <- function() {
+        session_dirs <- fs::dir_ls(here(SESS_DIR), type = "directory")
+        session_dates <- sapply(as.character(session_dirs), function(d) {
+            read_yaml(here(d, "session.yaml"))$date
+        }, USE.NAMES = FALSE)
+        sortindices <- order(session_dates)
+        session_ids <- basename(session_dirs)[sortindices]
+        session_dates <- session_dates[sortindices]
+
+        session_opts <- session_ids
+        names(session_opts) <- sprintf("%s – %s", session_dates, session_ids)
+
+        state$available_sessions <- session_opts
+    }
+
+    sessionDeleteModal <- function(sess_id) {
+        bodytext <- sprintf("Do you really want to delete the session %s? All data for this session will be lost.
+                            This cannot be undone.", sess_id)
+
+        modalDialog(title = "Delete this session?",
+                    bodytext,
+                    footer = tagList(
+                        modalButton("Cancel"),
+                        actionButton("deleteSessionConfirmed", "OK")
+                    )
+        )
+    }
 
     observeEvent(input$createSession, {
         sess_id <- NULL
         while (is.null(sess_id) || fs::dir_exists(here(SESS_DIR, sess_id))) {
-            sess_id <- stri_rand_strings(1, 12)
+            sess_id <- stri_rand_strings(1, SESS_ID_CODE_LENGTH)
         }
 
         fs::dir_create(here(SESS_DIR, sess_id))
@@ -56,29 +92,49 @@ server <- function(input, output) {
     })
 
     observeEvent(input$sessionsSelect, {
+        req(input$sessionsSelect)
         state$sess <- load_sess_config(input$sessionsSelect)
     })
 
-    output$sessionsList <- renderUI({
-        session_dirs <- fs::dir_ls(here(SESS_DIR), type = "directory")
-        session_dates <- sapply(as.character(session_dirs), function(d) {
-            read_yaml(here(d, "session.yaml"))$date
-        }, USE.NAMES = FALSE)
-        sortindices <- order(session_dates)
-        session_ids <- basename(session_dirs)[sortindices]
-        session_dates <- session_dates[sortindices]
-
-        session_opts <- session_ids
-        names(session_opts) <- sprintf("%s – %s", session_dates, session_ids)
-
-        selectInput("sessionsSelect", "Load session:", session_opts, selected = state$sess$sess_id)
+    observeEvent(input$deleteSession, {
+        req(state$sess)
+        showModal(sessionDeleteModal(state$sess$sess_id))
     })
 
-    output$activeSession <- renderUI({
+    observeEvent(input$deleteSessionConfirmed, {
+        req(state$sess)
+
+        # directory deletion is a very sensitive operation; perform some checks beforehand
+        stopifnot("session ID must be valid" =
+                      (nchar(state$sess$sess_id) == SESS_ID_CODE_LENGTH) && grepl("^[A-Za-z0-9]+$", state$sess$sess_id))
+        sess_dir <- here(SESS_DIR, state$sess$sess_id)
+        stopifnot("session path must point to directory" = fs::is_dir(sess_dir))
+        stopifnot("a session configuration must exist" = fs::file_exists(here(sess_dir, "session.yaml")))
+
+        # remove the session directory
+        fs::dir_delete(sess_dir)
+
+        updateAvailSessions()
+
+        removeModal()
+    })
+
+    output$sessionsList <- renderUI({
+        updateAvailSessions()
+
+        selectInput("sessionsSelect", "Load session:", state$available_sessions, selected = state$sess$sess_id)
+    })
+
+    output$activeSessionTitle <- renderText({
+        req(state$sess)
+
+        sprintf("Session %s", state$sess$sess_id)
+    })
+
+    output$activeSessionContent <- renderUI({
         req(state$sess)
 
         list(
-            h1(sprintf("Session %s", state$sess$sess_id)),
             tags$ul(
                 tags$li(sprintf("Date: %s", state$sess$date)),
                 tags$li(sprintf("Language: %s", state$sess$language)),
