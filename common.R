6226ceb6be78b270241b6985ad32f7087c486607
Markus Konrad <post@mkonrad.net>
2024-03-06 18:12:48 +0100

    code doc. common.R

--- common.R
+++ common.R
@@ -1,3 +1,9 @@
+# Shrager Memory Game – common functions and variables.
+#
+# Functions and variables used in both Shiny apps (the participant and the teacher app).
+#
+# Author: Markus Konrad <markus.konrad@htw-berlin.de>
+
 SESS_ID_CODE_LENGTH <- 8
 USER_ID_CODE_LENGTH <- 32
 SESS_DIR <- fs::path_abs(here("..", "sessions"))
@@ -7,6 +13,9 @@ STAGES <- c("start", "directions", "questions", "survey", "results", "end")
 GROUPS <- c("ctrl", "treat")
 
 
+# function that validates a single ID `id` for an expected length `expected_length` and optionally checks that there's
+# a folder in the session directory with that ID if `expect_session_dir` is TRUE; also checks that the ID only
+# consists of alphanumeric characters
 validate_id <- function(id, expected_length, expect_session_dir = FALSE) {
     if (length(id) != 1) return(FALSE)
     if (nchar(id) != expected_length) return(FALSE)
@@ -21,14 +30,19 @@ validate_id <- function(id, expected_length, expect_session_dir = FALSE) {
     TRUE
 }
 
+# save a session configuration (a list object) to the session.yaml file in the respective folder inside the
+# "sessions" directory
 save_sess_config <- function(sess) {
     write_yaml(sess, here(SESS_DIR, sess$sess_id, "session.yaml"))
 }
 
+# load a session configuration (a list object) from the respective session.yaml file
 load_sess_config <- function(sess_id) {
     read_yaml(here(SESS_DIR, sess_id, "session.yaml"))
 }
 
+# return the labels for the survey of session `sess` as character vector if a survey is defined for that session;
+# otherwise return NULL
 survey_labels_for_session <- function(sess) {
     if (sess$config$survey) {
         sapply(sess$survey, function(item) {
@@ -39,7 +53,10 @@ survey_labels_for_session <- function(sess) {
     }
 }
 
+# load all user data for a session `sess_id`; control which survey data to load via `survey_labels`; use function
+# `survey_labels_for_session()` to determine all survey labels
 data_for_session <- function(sess_id, survey_labels) {
+    # load participants data as list
     user_files <- fs::dir_ls(here(SESS_DIR, sess_id), type = "file", regexp = "user_[A-Za-z0-9]+.rds$")
     user_data <- lapply(user_files, function(f) {
         u <- readRDS(f)
@@ -53,20 +70,26 @@ data_for_session <- function(sess_id, survey_labels) {
     # filter user data: take only participants that have submitted answers
     user_data <- user_data[!sapply(user_data, is.null)]
 
+    # no user data – return empty data frame
     if (length(user_data) == 0) {
         return(data.frame(group = factor(levels = GROUPS),
                           n_correct = integer()))
     }
 
+    # user group assignments as char. vector
     group <- sapply(user_data, function(u) {
         u$group
     })
+
+    # num. correct answers as int. vector
     n_correct <- sapply(user_data, function(u) {
         sum(u$user_results)
     })
 
+    # build the resulting data frame
     res <- data.frame(group = group, n_correct = n_correct, row.names = 1:length(group))
 
+    # add survey answers as additional columns if necessary
     if (!is.null(survey_labels)) {
         survey_answers <- sapply(user_data, function(u) {
             ifelse(is.null(u$survey_answers), rep(NA, length(survey_labels)), u$survey_answers)
@@ -83,10 +106,13 @@ data_for_session <- function(sess_id, survey_labels) {
         res <- cbind(res, survey_answers)
     }
 
+    # set group as factor variable and return
     res$group <- factor(res$group, GROUPS)
     res
 }
 
+# check if a user provided answer `user_answer` is correct by matching it with the solution patterns defined in the
+# question definition `questiondef`
 check_answer <- function(questiondef, user_answer) {
     solutions <- questiondef$a
     is_regex <- !is.null(questiondef$regex) && questiondef$regex
